c2e3a3cf7f907c1a61957f52eabec73b30374836
Ettien Etienne BEKOIN <e.bekoin@cdci.ci>
2026-02-22 16:27:20 +0000

    creation de APi avec mixins

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from api.api.api import product_api_view
-
+from api.api.mixins import ProductListApiView, ProductCreateApiView, ProductDetailApiView, ProductUpdateApiView, ProductDeleteApiView, CombineApiViewSet
 
 app_name = "api"
 
@@ -10,5 +10,13 @@ urlpatterns = [
     path("products/", product_api_view, name="product_api_view"),
     path("products/<int:pk>/", product_api_view, name="product_api_view_detail"),
     path("", include("api.routers")),
+    path("v2/products-list/", ProductListApiView.as_view(), name="product_list_api_view"),
+    path("v2/products-create/", ProductCreateApiView.as_view(), name="product_create_api_view"),
+    path("v2/products-detail/<int:pk>/", ProductDetailApiView.as_view(), name="product_detail_api_view"),
+    path("v2/products-update/<int:pk>/", ProductUpdateApiView.as_view(), name="product_update_api_view"),
+    path("v2/products-delete/<int:pk>/", ProductDeleteApiView.as_view(), name="product_delete_api_view"),
+    path("v3/products-combine/", CombineApiViewSet.as_view(), name="product_combine_api_view"),
+    path("v3/products-combine/<int:pk>/", CombineApiViewSet.as_view(), name="product_combine_api_view_detail"),
+    
     
 ]
